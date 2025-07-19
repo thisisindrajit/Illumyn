12326e6aac2c3761bdbcda3c5d12ef44c30fcfc5
Indrajit <43838718+thisisindrajit@users.noreply.github.com>
2025-07-19 20:10:18 +0530

    added favicon

--- frontend/src/app/components/TopBar.tsx
+++ frontend/src/app/components/TopBar.tsx
@@ -4,20 +4,39 @@ import { Button } from "@/components/ui/button";
 import { APP_NAME } from "../../../constants/common";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const TopBar = () => {
   const pathname = usePathname();
+
+  const showLoginOrUserButton = () => {
+    switch (pathname) {
+      case "/":
+        return (
+          <Link href="/login">
+            <Button variant="outline" size="lg" className="px-4 py-2 border border-primary hover:text-background hover:bg-primary transition-colors text-sm animate-fade-in rounded-none">
+              Login
+            </Button>
+          </Link>
+        )
+      case "/user/dashboard":
+        return (
+          <Avatar className="size-10">
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        )
+      default:
+        return null;
+    }
+  }
 
   return (
     <div className="w-full flex justify-between items-center">
       <Link href="/">
         <div className="text-sm uppercase tracking-widest border border-foreground/25 h-10 px-4 py-2 animate-fade-in">{APP_NAME}</div>
       </Link>
-      {pathname !== "/login" && <Link href="/login">
-        <Button variant="outline" size="lg" className="px-4 py-2 border border-primary hover:text-background hover:bg-primary transition-colors text-sm animate-fade-in rounded-none">
-          Login
-        </Button>
-      </Link>}
+      {showLoginOrUserButton()}
     </div>
   );
 };
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -18,6 +18,12 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link href="https://api.fontshare.com/v2/css?f[]=general-sans@200,201,300,301,400,401,500,501,600,601,700,701&display=swap" rel="stylesheet" />
+        <link rel="icon" type="image/png" href="/favicons/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicons/favicon.svg" />
+        <link rel="shortcut icon" href="/favicons/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="Illumyn" />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
       </head>
       <body className={`antialiased`}>
         {/* Background decorative elements */}
